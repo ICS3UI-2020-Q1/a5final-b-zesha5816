2e1ce3bd2d338f471e467e6615d6a71dc83e3d94
zesha5816 <71140785+zesha5816@users.noreply.github.com>
2020-10-07 11:56:16 -0400

    Update Main.java
    finished the example

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that will ask the user for the last number possible to count to, starting   from 1. Your program will then calculate the sum of all of the odd numbers from 1 to that entered number.
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create the Scanner
+		Scanner input = new Scanner(System.in);
+		//output message to user
+		System.out.println("Please enter the maximum number to sum to:");
+		//get input from user
+		int userInput = input.nextInt();
+		//intialize the sum varaible as 0
+		int sum = 0;
+		//a for loop keeps looping until gets to the the input value 
+		for(int i = 1; i <= userInput; i++) {
+			// Check if the remainder is 0 if it does meaning its even so we skip the number.
+    	if(i % 2 == 0) {
+			//else if the remainder is not 0 means an odd number
+	  	}else {
+			//odd number add up the oddnumbers to the previous one
+			  sum += i;
+		  }
+  	}	
+		//output the answer to the user 
+   	System.out.println("The sum of the odd numbers is " + sum);
   }
 }
+
